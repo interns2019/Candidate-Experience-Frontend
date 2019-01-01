6e5bf580b7e9be64775d46a3b0a74f55c1681089
smrngosai <30699552+smrngosai@users.noreply.github.com>
2019-01-01 21:26:02 +0530

    UI done

--- CandidateApp/src/app/app.module.ts
+++ CandidateApp/src/app/app.module.ts
@@ -19,7 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatStepperModule} from '@angular/material/stepper';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +35,9 @@ import {MatButtonModule} from '@angular/material/button';
     YearlyAnalysisComponent,
     CommentAnalysisComponent
   ],
-  imports: [
+  imports: [MatListModule,MatDividerModule,MatStepperModule,
+    MatCardModule,
+    MatButtonToggleModule,
     MatButtonModule,
     MatInputModule,
     BrowserModule,
